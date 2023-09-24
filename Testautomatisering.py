dd70a8251a4bb30b09b68ae2aa5c2d3937ce080b
Simon Jäger <simonjager@hotmail.com>
2023-09-24 16:38:30 +0200

    satt upp testmiljö

--- Testautomatisering.py
+++ Testautomatisering.py
@@ -1,6 +1,8 @@
-import time
-
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
+from selenium.webdriver.support.wait import WebDriverWait
+
 
 
 driver = webdriver.Chrome()  # Optional argument, if not specified will search path.

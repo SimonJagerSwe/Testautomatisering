b98734a3db69eab96f31f919b960ec92a0be4a81
Simon Jäger <simonjager@hotmail.com>
2023-09-29 14:51:24 +0200

    ändrade från lägg till som favorit till lägg till i kundkorg för att slippa inloggning

--- test_testautomatisering.py
+++ test_testautomatisering.py
@@ -29,13 +29,24 @@ def test_1(load_driver):
     boolean_assert("malmomusikaffar" in driver.current_url, f"Expected malmomusikaffar in url, got: {driver.current_url}")
 
 
-# Kontrollera att det går att lägga till en vara i favorit-listan
+# Kontrollera att det går att lägga till Eastman T59/V Antique Red i kundkorg
 def test_2(load_driver):
     driver = load_driver
+
+    # Hämta elgitarrsida
     driver.get(electric_guitars)
+
+    # Identifiera rätt gitarr och klicka på länken
     find_guitar = driver.find_element(By.XPATH, '//*[@id="amasty-shopby-product-list"]/div[2]/ol/li[39]/div/div/header/a')
-    # time.sleep(5)
     driver.execute_script("arguments[0].click();", find_guitar)
+
+    # Lägg till i kundkorg
+    add_to_cart = driver.find_element(By.XPATH, '//*[@id="product-addtocart-button"]')
+    driver.execute_script("arguments[0].click();", add_to_cart)
+
+    # Gå till kundkorg och kontrollera att rätt produkt ligger där
+    in_cart = driver.find_element(By.XPATH, '//*[@id="html-body"]/div[4]/header/div[2]/div[1]/a')
+    driver.execute_script("arguments[0].click();", in_cart)
 
 # Kontrollera att facebooklänken stämmer
 def test_3(load_driver):
